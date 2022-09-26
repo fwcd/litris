529314942cd55e341f474789819abe320eb9122c
fwcd <fwcdmail@gmail.com>
2022-09-26 15:37:02 +0200

    Update to lighthouse-client 0.3.0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use lighthouse_client::{Authentication, Lighthouse, Result};
+use tracing_subscriber::EnvFilter;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .compact()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
+    let username = env::var("LIGHTHOUSE_USER").unwrap();
+    let token = env::var("LIGHTHOUSE_TOKEN").unwrap();
+    let auth = Authentication::new(&username, &token);
+    
+    let mut lh = Lighthouse::connect_with_tokio(auth).await?;
+    let stream = lh.stream_model().await?;
+
+    // TODO
+
+    Ok(())
 }
